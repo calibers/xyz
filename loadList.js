0ec82acfd72b5c1274288edb826f0efefc2136bd
hamstlanda <hamstlanda@gmail.com>
2016-08-11 11:30:57 -0400

    changed the responsiveness of the website

--- loadList.js
+++ loadList.js
@@ -1,8 +1,26 @@
-function getLocation(tag){
-    return window.location.hostname + "?"+tag;
+//displays all the orgs that have the tag described in the url
+function displayTag(){
+    var tag = window.location.hash.slice(1);
+    if(tag!=""){
+        $(".org").hide();
+        $("."+tag).show();
+        grey("."+tag+"1");
+    }else {
+        $(".org").show();
+    }
 }
-
-
+//adds class active to the appropriate button
+function grey(className){
+    $(".orgBtn").removeClass('active')
+    $(className).addClass('active')
+}
+/*works when a button is clicked, changes the url and calls display tag function*/
+$(document).on('click', 'button', function() {
+    window.location = $(this).parent().attr('href');
+    $(this).attr('data-tag');
+//    grey(this.className);
+    displayTag();
+});
 
 (function() {
     console.log( "ready!" );
@@ -17,32 +35,33 @@ function getLocation(tag){
             //put placeholder 
             if(val.Logo==''){
                  //image
-                 items.push("<div class='row'><div class='"+val.Tags+"'><div class='eight columns'><li id='"+key+"'><img src='"+'http://up.2sw2r.com/upfiles2/ewg45788.png'+"'></li>");
-             }else{
+                 items.push("<div class='row'><div class='org "+val.Tags+" on'><div class='twelve columns'><li id='"+key+"'><img src='"+'http://up.2sw2r.com/upfiles2/ewg45788.png'+"'></li>");
+            }else{
                  //image
-                 items.push("<div class='row'><div class='"+val.Tags+"'><div class='eight columns'><li id='"+key+"'><img src='"+val.Logo+"'></li>");  
+                 items.push("<div class='row'><div class='org "+val.Tags+" on'><div class='twelve columns'><li id='"+key+"'><img src='"+val.Logo+"'></li>");  
             }
             //name and description
            
             var myTags = val.Tags;
+            //if there is more than one tag it needs to loop through all the tags creating a new button for each
             if(myTags.indexOf(' ')>-1){
                 var arrayTag = myTags.split(' ');
                 items.push( "<li class='myText' id='" + key + "'><h5>"+val.Name+"</h5><p>"+ val.Description +"</p><div class='the-tags'>");
                 for(var n in arrayTag){
-                    items.push("<button class='"+arrayTag[n]+"1'onclick='myFunction(this.className)' >"+arrayTag[n]+"</button>");
+                    items.push("<a href='#"+arrayTag[n]+"'><button class='active "+arrayTag[n]+"1' >"+arrayTag[n]+"</button></a>");
                 }
             }else{
-                 items.push( "<li class='myText' id='" + key + "'><h5>"+val.Name+"</h5><p>"+ val.Description +"</p><div class='the-tags'><button class='"+val.Tags+"1'onclick='myFunction(this.className)' >"+val.Tags+"</button>");
-            }
-           
-            
+                 items.push( "<li class='myText' id='" + key + "'><h5>"+val.Name+"</h5><p>"+ val.Description +"</p><div class='the-tags'><a href='#"+val.Tags+"'><button class='active "+val.Tags+"1'>"+val.Tags+"</button></a>");
+            }             
             //links 
-            items.push( "</div></li></div><li id='" + key + "'><div class='four columns'><ul class='linkss'><a href='" + val.Website + "' class='ss-icon' target='_blank'>"+lin+"</a></li><li class='link' id='" + key + "'><a href='" + val.Twitter+"' class='ss-icon' target='_blank'>"+'&#xF611;'+"</a></li><li class='link' id='" + key + "'><a href='" + val.LinkedIn +"'class='ss-icon'  target='_blank'>"+'&#xF612;'+"</a></div></ul></div></li></div></div>" );
+            items.push( "</div></li><li id='" + key + "'><ul class='links'><li class='link' id='website'><a href='" + val.Website + "' class='ss-icon' target='_blank'>"+lin+"</a></li><li class='link' id='twitter'><a href='" + val.Twitter+"' class='ss-icon' target='_blank'>"+'&#xF611;'+"</a></li><li class='link' id='linkedin'><a href='" + val.LinkedIn +"'class='ss-icon'  target='_blank'>"+'&#xF612;'+"</a></div></ul></li></div></div></div>" );
             
         });
             $( "<ul/>", {
                  "class": "my-new-list",
                  html: items.join( "" )
              }).appendTo( ".orgList" );
+        //when the page loads it checks the url to sort based on the address
+            displayTag();
     });
 })();        
--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function showAll(){
-    $(".women").show()
-    $(".lgbt").show()
-    $(".poc").show()
-}
-function hideAll(){
-    $(".women").hide()
-    $(".lgbt").hide()
-    $(".poc").hide()
-}
+
 //function sets the color of the button to grey
 function greyOut(i, len,elements){
      for (i = 0, len = elements.length; i < len; i++) {
@@ -16,7 +7,8 @@ function greyOut(i, len,elements){
             }
 }
 //counters for clicks on each of the classes of buttons
-var clickW=0, clickP=0, clickL=0;
+var clickW=0, clickP=0, clickL=0, clickA=0;
+
 //function that changes the colors of the buttons
 function setColor(btn) {
     var elements, i, len;
@@ -69,11 +61,35 @@ function setColor(btn) {
         }
         elements = document.getElementsByClassName(btn);
         greyOut(i, len,elements);
+    }else if(btn=='all'){
+        if(clickW==1){
+            elements = document.getElementsByClassName('women1');
+            for (i = 0, len = elements.length; i < len; i++) {
+                elements[i].style.color = 'white';
+                elements[i].style.background = '#ff6a2f';
+            }clickW=0;
+        }else if(clickP==1){
+            elements = document.getElementsByClassName('poc1');
+            for (i = 0, len = elements.length; i < len; i++) {
+                elements[i].style.color = 'white';
+                elements[i].style.background = '#33c3f0';
+            }clickP=0;
+        }else if(clickL==1){
+            elements = document.getElementsByClassName('lgbt1');
+            for (i = 0, len = elements.length; i < len; i++) {
+                elements[i].style.color = 'white';
+                elements[i].style.background = '#57e257';
+            }clickP=0;
+        }
+        elements = document.getElementsByClassName(btn);
+        greyOut(i, len,elements);
     }
 }
 
+/*
+
 //function that sorts organizations by tag when tag is clicked
- function myFunction(className){
+ /*function myFunction(className){
     hideAll();
     if(className=='women1'){
         clickW=1;
@@ -84,7 +100,7 @@ function setColor(btn) {
         console.log("clicked on women");
     }else if(className=='poc1'){
         clickP=1;
-         $(".poc").show()
+        $(".poc").show()
         console.log("clicked on poc");
         document.getElementById("organizations").value='eth';
         //document.getElementById("eth").selected = "true";
@@ -96,21 +112,4 @@ function setColor(btn) {
         //document.getElementById("lgb").selected = "true";
     }
       setColor(className);
- }
-
-
-window.onload = function() {
-//when selector is changed sorts orgs by tag
-    $('.organizations').change(function(){
-        hideAll();
-        if($(this).find("option:selected").attr('value')=='fem'){
-            $(".women").show()
-        }else if($(this).find("option:selected").attr('value')=='eth'){
-            $(".poc").show()
-        }else if($(this).find("option:selected").attr('value')=='lgb'){
-            $(".lgbt").show()
-        }else if($(this).find("option:selected").attr('value')=='all'){
-            showAll()
-        }
-    });
-}    
+ }*/
