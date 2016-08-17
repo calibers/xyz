71777bf9677d85129544bf914c273172badbfade
hamstlanda <hamstlanda@gmail.com>
2016-08-17 19:19:19 -0400

    still testing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var router = express.Router();
 var path=__dirname +"/views/";
 var path1 =require("path");
+
+app.set('port', (process.env.PORT || 8000));
+
+
 router.get('/', function(req, res){
     res.sendFile(path+"index.html");
 });
@@ -12,6 +16,9 @@ app.use('/', router);
 app.use('/js',express.static(path1.join(__dirname, 'public/javascripts')));
 app.use('/css',express.static(path1.join(__dirname, 'public/stylesheets')));
 
-app.listen(8000, function(){
-    console.log("Live at port 8000");
+//app.listen(8000, function(){
+//    console.log("Live at port 8000");
+//});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
